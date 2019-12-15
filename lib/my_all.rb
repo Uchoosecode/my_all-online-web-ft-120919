ee7c9f70a4f1952b06a6c62a8084309708774d5b
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-15 00:15:23 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,11 +3,14 @@ require 'pry'
 def my_all?(collection)
   i = 0 
   my_collect = []
+   
     while i < collection.length 
      my_collect << yield(collection[i])
+  
   i += 1 
     end
-   if block_return.include?(false)
+    
+   if my_collect.include?(false)
      false
      
    else
